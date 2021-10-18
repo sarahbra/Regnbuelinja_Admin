c5b5e66331789121af6220bd935368f070c4c974
sarah <sarah@LAPTOP-BIUP68RE>
2021-10-18 16:24:27 +0200

    Startet på components for ferder, ruter, etc

---
+++ Regnbuelinja_Admin/ClientApp/src/app/rute/ruter.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Rute } from "../Rute";
+
+@Component({
+  templateUrl: "ruter.html"
+})
+
+export class Ruter {
+  alleRuter: Array<Rute>;
+  laster: boolean;
+
+  constructor(private http: HttpClient, private modalService: NgbModal) { }
+
+  ngOnInit() {
+    this.laster = true;
+    this.hentAlleRuter();
+  }
+
+  hentAlleRuter() {
+    this.http.get<Rute[]>("api/ruter/")
+      .subscribe(rutene => {
+        this.alleRuter = rutene;
+        this.laster = false;
+      },
+        error => console.log(error)
+      );
+  };
+}
