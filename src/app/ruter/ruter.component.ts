329df34e816ae54fb53b88cbfbba7b26f34d4d6e
Degetei <daniel.gulliksen@gmail.com>
2021-10-23 18:25:03 +0200

    added slettRuter display functionality; route id's now visible.

--- src/app/ruter/ruter.component.ts
+++ src/app/ruter/ruter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Rute } from '../models/rute';
+import { Ferd } from '../models/ferd';
 import { Router } from '@angular/router';
 import { SlettModal } from '../modals/slett.modal';
 
@@ -33,25 +34,77 @@ export class RuterComponent implements OnInit {
 
   endreRute(id: number) {}
 
-  slettRute(id: number) {}
-
   leggTilRute() {}
+
+  hentFerder(id: number) : string {
+    let svar = "Mislykket";
+    this._http.get<Ferd[]>('/api/admin/ferder/' + id).subscribe(
+      (ferdene) => {
+        console.log(ferdene);
+        console.log(ferdene.length)
+        if (ferdene.length == 0){
+          console.log("Ingen ferder funnet for rute " + id);
+        } else {
+          svar = "Rute " + id + " brukes av følgene ferd(er): ";
+          console.log("looping")
+          for (let i = 0; i < ferdene.length; i++){
+            if (i < ferdene.length -1){
+              svar += ferdene[i].baat +", ";
+            } else {
+              svar += ferdene[i].baat;
+            }
+          }
+        }
+        
+        
+      },
+      (error) => console.log(error)
+    );
+
+    return svar;
+  }
 
   visModalOgSlett(id: number) {
     console.log(id);
     const modalRef = this.modalService.open(SlettModal, {
       backdrop: 'static', keyboard: false
     });
-    const textBody: string = "Ruten med id "+ id;
-    //updated tsconfig.json
-    console.log(textBody);
-    modalRef.componentInstance.updateBody(textBody);
+
+    let textBody: string = "Ruten med id "+ id + "kan slettes. Vil du slette Rute " +id+"?";
+
+    this._http.get<Ferd[]>('/api/admin/ferder/' + id).subscribe(
+      (ferdene) => {
+        console.log(ferdene);
+        console.log(ferdene.length)
+        if (ferdene.length == 0){
+          modalRef.componentInstance.updateBody(textBody);
+          textBody += "Er du sikker "
+        } else {
+          textBody = "Rute " + id + " kan ikke slettes. Den brukes av følgene ferd(er): ";
+          console.log("looping")
+          for (let i = 0; i < ferdene.length; i++){
+            if (i < ferdene.length -1){
+              textBody += ferdene[i].id + ", ";
+            } else {
+              textBody += ferdene[i].id + ". ";
+            }
+          }
+
+          //updated tsconfig.json
+          textBody += "Slett gjeldene ferder for å kunne slette rute "+ id;
+          modalRef.componentInstance.updateBody(textBody);
+        }
+        
+        
+      },
+      (error) => console.log(error)
+    );
 
     modalRef.result.then(retur => {
         console.log('Lukket med:' + retur);
         if (retur == "Slett") {
           
-          this._http.delete("api/admin/" + id)
+          this._http.delete("api/admin/rute/slett/" + id)
             .subscribe(retur => {
               this.hentAlleRuter();
             },
