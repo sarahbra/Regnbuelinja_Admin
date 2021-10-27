2cc3d5d85409e4759928bfc3b9aa2cb8d4c6f225
vilderoxan <vilde.fauchald@gmail.com>
2021-10-27 23:53:32 +0200

    enkel styling

--- src/app/ferder/ferder.component.ts
+++ src/app/ferder/ferder.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Ferd } from '../models/ferd';
+import { SlettModal } from '../modals/slett.modal';
+import { AlertModal } from '../modals/alert.modal';
+import { Router } from '@angular/router';
 
 @Component({
   //selector: 'app-ruter', -> Det er routing som gjelder så denne gjør ingenting
@@ -12,7 +15,11 @@ export class FerderComponent implements OnInit {
   alleFerder: Array<Ferd> = [];
   laster: boolean = false;
 
-  constructor(private _http: HttpClient, private modalService: NgbModal) {}
+  constructor(
+    private _http: HttpClient,
+    private _router: Router,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.laster = true;
@@ -35,7 +42,36 @@ export class FerderComponent implements OnInit {
 
   endreFerd(id: number) {}
 
-  slettFerd(id: number) {}
+  visModalOgSlett(id: number) {
+    console.log(id);
+    const modalRef = this.modalService.open(SlettModal, {
+      backdrop: 'static',
+      keyboard: false,
+    });
+
+    let textBody: string = 'Vil du slette Ferd ' + id + '?';
+    modalRef.componentInstance.updateBody(textBody);
+
+    modalRef.result.then((retur) => {
+      console.log('Lukket med:' + retur);
+      if (retur == 'Slett') {
+        this._http.delete('/api/admin/ferd/' + id).subscribe(
+          () => {
+            this.hentAlleFerder();
+          },
+          (res) => {
+            const modalRef = this.modalService.open(AlertModal, {
+              backdrop: 'static',
+              keyboard: false,
+            });
+            let textBody: string = res.error;
+            modalRef.componentInstance.updateBody(textBody);
+          }
+        );
+      }
+      this._router.navigate(['/ferder']);
+    });
+  }
 
   leggTilFerd() {}
 }
