7a7de4efaf935afd112c9e469147a73dfc3d7968
vilderoxan <vilde.fauchald@gmail.com>
2021-10-27 12:25:47 +0200

    legger til kommentar i ferd.ts

--- src/app/models/ferd.ts
+++ src/app/models/ferd.ts
@@ -1,4 +1,8 @@
 export interface Ferd {
+  //Husk å hente rute ved utskrift ved id.
+  //Husk: Ved endring av Ferd må en drop down meny vises med alle ruter via et hentAlleRuter-kall
+  //Det samme gjelder for Båt.
+  //Når man sender inn endring av Ferd må man sende inn rute-id og båt-id
   id: number;
   baat: number;
   rute: number;
