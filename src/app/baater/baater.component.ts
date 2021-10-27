06732e000c372676ebf8a9cdba1c352d00e51063
vilderoxan <vilde.fauchald@gmail.com>
2021-10-27 13:36:44 +0200

    legger til slett-metode for baat

--- src/app/baater/baater.component.ts
+++ src/app/baater/baater.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Baat } from '../models/baat';
+import { Ferd } from '../models/ferd';
+import { Router } from '@angular/router';
+import { SlettModal } from '../modals/slett.modal';
 
 @Component({
   //selector: 'app-ruter', -> Det er routing som gjelder så denne gjør ingenting
@@ -12,7 +15,11 @@ export class BaaterComponent implements OnInit {
   alleBaater: Array<Baat> = [];
   laster: boolean = false;
 
-  constructor(private http: HttpClient, private modalService: NgbModal) {}
+  constructor(
+    private _http: HttpClient,
+    private _router: Router,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.laster = true;
@@ -20,26 +27,68 @@ export class BaaterComponent implements OnInit {
   }
 
   hentAlleBaater() {
-    this.http
-      //Endre til nytt endepunkt som henter alle båter.
-      .get<Baat[]>('/api/Bestilling/HentBaat', {
-        params: {
-          id: '1',
-          Startpunkt: 'Oslo',
-        },
-      })
-      .subscribe(
-        (baater) => {
-          this.alleBaater = baater;
-          this.laster = false;
-        },
-        (error) => console.log(error)
-      );
+    //Endre til nytt endepunkt som henter alle båter.
+    this._http.get<Baat[]>('/api/Bestilling/HentAlleBaater').subscribe(
+      (baater) => {
+        this.alleBaater = baater;
+        this.laster = false;
+      },
+      (error) => console.log(error)
+    );
   }
 
   endreBaat(id: number) {}
 
-  slettBaat(id: number) {}
+  visModalOgSlett(id: number) {
+    console.log(id);
+    const modalRef = this.modalService.open(SlettModal, {
+      backdrop: 'static',
+      keyboard: false,
+    });
+
+    let textBody: string =
+      'Båt med id ' + id + 'kan slettes. Vil du slette Båt ' + id + '?';
+
+    this._http.get<Ferd[]>('/api/admin/ferder/' + id).subscribe(
+      (ferdene) => {
+        console.log(ferdene);
+        console.log(ferdene.length);
+        if (ferdene.length == 0) {
+          modalRef.componentInstance.updateBody(textBody);
+          textBody += 'Er du sikker ';
+        } else {
+          textBody =
+            'Båt ' + id + ' kan ikke slettes. Den brukes av følgene ferd(er): ';
+          console.log('looping');
+          for (let i = 0; i < ferdene.length; i++) {
+            if (i < ferdene.length - 1) {
+              textBody += ferdene[i].id + ', ';
+            } else {
+              textBody += ferdene[i].id + '. ';
+            }
+          }
+
+          //updated tsconfig.json
+          textBody += 'Slett gjeldene ferder for å kunne slette båt ' + id;
+          modalRef.componentInstance.updateBody(textBody);
+        }
+      },
+      (error) => console.log(error)
+    );
+
+    modalRef.result.then((retur) => {
+      console.log('Lukket med:' + retur);
+      if (retur == 'Slett') {
+        this._http.delete('api/admin/baat/slett/' + id).subscribe(
+          (retur) => {
+            this.hentAlleBaater();
+          },
+          (error) => console.log(error)
+        );
+      }
+      this._router.navigate(['/baater']);
+    });
+  }
 
   leggTilBaat() {}
 }
