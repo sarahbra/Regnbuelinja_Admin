f27cb0570b9308ed7c7b61a7a60d2b2e6336d8b4
chonlawitwith-pettersen <s311596@oslomet.no>
2021-10-28 09:52:06 +0200

    lagt til implements

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,44 +5,44 @@ import { Router } from '@angular/router';
 import { Bruker } from '../models/bruker';
 
 @Component({
-    templateUrl: './login.component.html',
+  templateUrl: './login.component.html',
 })
-export class LoginComponent {
-    loginSkjema: FormGroup;
-    loginError: boolean;
+export class LoginComponent implements OnInit {
+  loginSkjema: FormGroup;
+  loginError: boolean;
 
 
-    validering = {
-        brukernavn: [null, Validators.compose([Validators.required, Validators.pattern('[a-zA-ZøæåØÆÅ\\-. ]{2,30}')],),
-        ],
-        passord: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-ZæøåÆØÅ])([a-zA-ZæøåÆØÅ0-9]+){6,}$')])],
-    };
+  validering = {
+    brukernavn: [null, Validators.compose([Validators.required, Validators.pattern('[a-zA-ZøæåØÆÅ\\-. ]{2,30}')],),
+    ],
+    passord: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-ZæøåÆØÅ])([a-zA-ZæøåÆØÅ0-9]+){6,}$')])],
+  };
 
-    constructor(private http: HttpClient, private fb: FormBuilder, private router: Router) {
-        this.loginSkjema = fb.group(this.validering);
+  constructor(private http: HttpClient, private fb: FormBuilder, private router: Router) {
+    this.loginSkjema = fb.group(this.validering);
 
-    }
+  }
 
-    ngOnInit() {
-        this.loginError = false;
-    }
+  ngOnInit() {
+    this.loginError = false;
+  }
 
 
-    vedSubmit() {
-        const brukernavn = this.loginSkjema.value.brukernavn;
-        const passord = this.loginSkjema.value.passord;
-        const login = new Bruker(brukernavn, passord);
+  vedSubmit() {
+    const brukernavn = this.loginSkjema.value.brukernavn;
+    const passord = this.loginSkjema.value.passord;
+    const login = new Bruker(brukernavn, passord);
 
-        this.http.post('/api/admin/logg_inn', login).subscribe(
-            (ok) => {
-                if (ok) {
-                    this.router.navigate(['/ruter']);
-                } else {
-                    this.loginError = true;
-                }
+    this.http.post('/api/admin/logg_inn', login).subscribe(
+      (ok) => {
+        if (ok) {
+          this.router.navigate(['/ruter']);
+        } else {
+          this.loginError = true;
+        }
 
-            },
-            (error) => console.log(error)
-        );
-    }
+      },
+      (error) => console.log(error)
+    );
+  }
 }
