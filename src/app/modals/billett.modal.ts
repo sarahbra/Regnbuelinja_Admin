9b57ecde5557a39227606efad3c3617d23b980df
vilderoxan <56063879+vilderoxan@users.noreply.github.com>
2021-10-28 17:20:14 +0200

    modal dersom billett ikke kan slettes fordi den er ubetalt er lagt til (#11)

--- src/app/modals/billett.modal.ts
+++ src/app/modals/billett.modal.ts
@@ -3,9 +3,12 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Billett } from '../models/billett';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BestillingUbetaltModal } from './bestillingUbetalt.modal';
 
 //Dette er en modal som lister de billettene som er knyttet til en rute, ferd, båt eller bestilling
-//som må slettes FØR man kan slette de nevnte entitetene
+//billettene som er linket må slettes FØR man kan slette de nevnte entitetene.
+//Dette for å sikre at en kunde som har betalt for en reise ikke ender opp med en tom ferd, rute, uten bår etc..
 
 @Component({
   templateUrl: 'billett.modal.html',
@@ -18,13 +21,13 @@ export class BillettModal implements OnInit {
   }
   alleBilletter: Array<Billett> = [];
   laster: boolean;
-  voksen: boolean;
   @Input() dataToTakeAsInput: number;
 
   constructor(
     private _http: HttpClient,
     private _router: Router,
-    public modal: NgbActiveModal
+    public modal: NgbActiveModal,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit() {
@@ -48,6 +51,18 @@ export class BillettModal implements OnInit {
   }
 
   slettBillett(id: number) {
-    //HVIS bestilling er ubetalt så vis feilmelding om at Bestilling er ubetalt og kan ikke slettes
+    this._http.delete('/api/admin/billett/' + id).subscribe((ok) => {
+      this.hentAlle();
+    }),
+      //Dersom billetten ikke kan slettes er det fordi den er ubetalt -> modal vises med info og lukk knapp. Admin må håndtere betaling først som ikke er den del av vår løsning
+      (err: string) => {
+        const modalRef = this.modalService.open(BestillingUbetaltModal, {
+          backdrop: 'static',
+          keyboard: false,
+        });
+        let textBody: string = err;
+        modalRef.componentInstance.updateBody(textBody);
+      };
+    this._router.navigate(['/ruter']);
   }
 }
