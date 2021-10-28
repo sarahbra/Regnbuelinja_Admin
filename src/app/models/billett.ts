8603c75b3314d1d24a161a34bc7a7e6d06503070
vilderoxan <vilde.fauchald@gmail.com>
2021-10-28 10:28:42 +0200

    endrer syntaks på felter

--- src/app/models/billett.ts
+++ src/app/models/billett.ts
@@ -1,7 +1,12 @@
 export interface Billett {
   //Husk å hente ferd ved utskrift ved id.
   //En bestilling kan ha mange billetter. Billett er ikke en enhet i menyen
+<<<<<<< HEAD
   biId: number;
   fId: number;
+=======
+  id: number;
+  ferd: number;
+>>>>>>> Henter ruter, båter , ferder og sletter  (#6)
   voksen: boolean;
 }
