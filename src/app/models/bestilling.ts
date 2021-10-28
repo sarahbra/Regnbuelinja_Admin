34327e8be7b2221bc33104e461b04c24afb8ac70
vilderoxan <vilde.fauchald@gmail.com>
2021-10-28 10:05:18 +0200

    småendringer

--- src/app/models/bestilling.ts
+++ src/app/models/bestilling.ts
@@ -1,7 +1,10 @@
 import { Billett } from './billett';
 
 export interface Bestilling {
-  id: number;
-  totalpris: number;
+  beId: number;
+  kId: number;
+  totalPris: number;
+  betalt: boolean;
+  //NB! må kjøre kall for å hente denne når knappen trykkes
   billettliste: Billett[];
 }
