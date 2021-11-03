c320b3d42d66f06a17a1b646ad30092ab6c32881
Degetei <daniel.gulliksen@gmail.com>
2021-11-03 22:30:14 +0100

    legg til dropdown (ferd)

--- src/app/ferder/legg_tilFerd.modal.ts
+++ src/app/ferder/legg_tilFerd.modal.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl, Validators, FormBuilder, Form } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Baat } from '../models/baat';
+import { Rute } from '../models/rute';
 import { Ferd } from '../models/ferd';
 
 @Component({
@@ -10,20 +12,26 @@ import { Ferd } from '../models/ferd';
 export class LeggTilFerdModal {
     forms: FormGroup;
     fb: FormBuilder = new FormBuilder();
+    alleBaater: Array<Baat> = [];
+    alleRuter: Array<Rute> = [];
 
     allForms = {
-        bIdForm: [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])],
-        rIdForm: [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])],
+        //bIdForm: [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])],
+        //rIdForm: [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])],
+        bIdForm: [null, Validators.required],
+        rIdForm: [null, Validators.required],
         avreiseDatoForm: [null, Validators.compose([Validators.required, Validators.pattern(/(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/)])],
         avreiseKlokkeslettForm: [null, Validators.compose([Validators.required, Validators.pattern(/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/)])],
         ankomstDatoForm: [null, Validators.compose([Validators.required, Validators.pattern(/(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/)])],
         ankomstKlokkeslettForm: [null, Validators.compose([Validators.required, Validators.pattern(/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/)])]
     }
 
-    constructor(public modal: NgbActiveModal, private http: HttpClient){ }
+    constructor(public modal: NgbActiveModal, private _http: HttpClient){ }
 
     ngOnInit() {
         this.forms = this.fb.group(this.allForms);
+        this.hentAlleRuter();
+        this.hentAlleBaater();
     }
   
     vedSubmit() {
@@ -40,7 +48,7 @@ export class LeggTilFerdModal {
       const ferd = new Ferd(bId, rId, avreiseTid, ankomstTid);
       //const rute = null;
 
-      this.http.post('/api/admin/ferder', ferd).subscribe(
+      this._http.post('/api/admin/ferder', ferd).subscribe(
         (ok) => {
           if (ok)
             this.modal.close("Vellykket");
@@ -54,5 +62,23 @@ export class LeggTilFerdModal {
         }
       );
 
+    }
+
+    hentAlleBaater() {
+      this._http.get<Baat[]>('/api/admin/baater').subscribe(
+        (baater) => {
+          this.alleBaater = baater;
+        },
+        (error) => console.log(error)
+      );
+    }
+  
+    hentAlleRuter() {
+      this._http.get<Rute[]>('/api/admin/ruter').subscribe(
+        (rutene) => {
+          this.alleRuter = rutene;
+        },
+        (error) => console.log(error)
+      );
     }
 }
