0106fb74ecf60b6b55e1b1981a5d0877ad493349
vilderoxan <56063879+vilderoxan@users.noreply.github.com>
2021-11-04 11:05:05 +0100

    legger til ferdRute.ts (#32)

---
+++ src/app/models/ferdRute.ts
@@ -0,0 +1,21 @@
+//En klasse for å vise eventuell mulig returferder for en bestilling med enveisbillett
+//Det er i dette tilfellet mulig å legge til returbillett og da må vi hente mulige returferder basert på avreiseferd sin dato og klokkeslett
+// Det er altså for å forhindre at kunden kan bestille returreise mens kunden er midt på havet på en annen reise
+export class FerdRute {
+  fId: number;
+  avreisetid: string;
+  ankomsttid: string;
+  strekning: string;
+
+  constructor(
+    fId: number,
+    avreisetid: string,
+    ankomsttid: string,
+    strekning: string
+  ) {
+    this.fId = fId;
+    this.avreisetid = avreisetid;
+    this.ankomsttid = ankomsttid;
+    this.strekning = strekning;
+  }
+}
